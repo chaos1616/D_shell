ceb74f1c4aeee83014715ceb13dc6698476f65fa
chaos1616 <hp74237@gmail.com>
2025-10-08 12:09:16 +0100

    update

--- src/main.c
+++ src/main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-int main(){
+int main(void){
+   unsigned char buffer[2048];
+   char delim = ' ';
+   char *delimptr = &delim;
 
+   putchar('>');
+   
+   char *err = fgets(buffer, 2048, stdin);
+   if(err == NULL){
+      printf("Error reading file!\n");
+      return 1;
+   }
+   
+   for(char * token = 0; token != NULL; ){
+      token = strtok(buffer, delimptr);
+      if(strcmp(token, "ls") == 0){
+         
+      }
+      
    return 0;
+}
+
+void runcommand(){
+   pid_t child = fork();
+   waitpid();
+
+
 }
